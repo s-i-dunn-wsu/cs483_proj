91ea2514700b41f90025faad62fecbf54117ed33
Samuel Dunn <samuel.i.dunn@wsu.edu>
2019-10-31 10:21:01 -0700

    Fall back to a more robust way of finding IDs within a page. Only apply it to finding the name for now, as it will drastically reduce performance

--- mtg_qe/scraper/card_extractor.py
+++ mtg_qe/scraper/card_extractor.py
@@ -1,6 +1,7 @@
 # Samuel Dunn
 # CS 483 Fall2019
 
+import re
 from enum import Enum, auto
 from requests.compat import urljoin
 
@@ -30,6 +31,20 @@ class ContentRowIds:
     Number = "ctl00_ctl00_ctl00_MainContent_SubContent_SubContent_numberRow"
     Artist = "ctl00_ctl00_ctl00_MainContent_SubContent_SubContent_artistRow"
 
+contentIdDefaults = {
+    'nameRow' : ContentRowIds.Name,
+    'manaRow' : ContentRowIds.Mana,
+    'cmcRow'  : ContentRowIds.CMC,
+    'typeRow' : ContentRowIds.Type,
+    'ptRow'   : ContentRowIds.PT,
+    'textRow' : ContentRowIds.Text,
+    'setRow'  : ContentRowIds.Expac,
+    'flavorRow': ContentRowIds.Flavor,
+    'rarityRow': ContentRowIds.Rarity,
+    'numberRow': ContentRowIds.Number,
+    'artistRow': ContentRowIds.Artist
+}
+
 class CardExtractor(object):
     """
     This class manages taking a beautiful soup object of a card's page
@@ -45,6 +60,26 @@ class CardExtractor(object):
         # Pages will typically have a JS section wherein it defines all
         # the IDs (function ClientIDs() {}; ClientIDs.nameRow='...' kind
         # of stuff.)
+        self._content_name_id = re.search(r'ClientIDs.nameRow', str(page_soup))
+
+    def identify_id(self, str_id):
+        """
+        Takes a common string and identifies the id string used to identify
+        the target element within page_soup.
+        :param str str_id: the common string for the target id.
+        :rtype: str
+        :return: the id string used for the desired element.
+
+        example:
+        identify_id('nameRow') will find the id:
+        'ctl00_ctl00_ctl00_MainContent_SubContent_SubContent_nameRow' on *most* pages.
+        """
+        rs = r'ClientIDs.{}\s*=\s*[\'"](.*)[\'"]'.format(str_id)
+        m = re.match(rs, str(self._page_soup))
+        if m:
+            return m.group(1)
+        else:
+            return contentIdDefaults[str_id]
 
     def extract(self):
         """
@@ -63,13 +98,12 @@ class CardExtractor(object):
         def extract_mana(sub_soup):
             return [x['alt'] for x in sub_soup.children if not isinstance(x, str)]
 
-
         card = Card()
 
         card.gatherer_link = self._link
         card.multiverseid = self._multiverseid
         card.external_artwork = self._page_soup.find('img', id='ctl00_ctl00_ctl00_MainContent_SubContent_SubContent_cardImage')['src']
-        card.name = extract_text(ContentRowIds.Name, False)
+        card.name = extract_text(self.identify_id('nameRow'), False)
         card.type = extract_text(ContentRowIds.Type, False)
 
         if not 'land' in card.type.lower():
