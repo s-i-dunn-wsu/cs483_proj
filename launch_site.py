ef741029d1c12e38d63c589f37b572ebcaa0cdff
Samuel Dunn <samuel.i.dunn@wsu.edu>
2019-12-06 11:10:13 -0800

    add transform_data, rename bootstrap.py

--- launch_site.py
+++ launch_site.py
@@ -1,7 +1,9 @@
+#!/usr/bin/env python3
+
 # Samuel Dunn
 # CS 483, Fall 2019
 
-# Quick script to launch the cherrypy engine.
+# Quick script to launch the cherrypy engine to serve our content.
 
 from mtg_qe.site.main import main
 
--- run_scrape.py
+++ run_scrape.py
@@ -1,5 +1,7 @@
-# This is a quick and dirty script to launch the scraper
-# in a dev environment
+#!/usr/bin/env python3
+
+# This script will run the projects scraper
+# with out requiring the package to be installed.
 
 import sys
 import logging
@@ -7,10 +9,12 @@ import time
 from mtg_qe.scraper import cli_entry
 
 stime = time.strftime("%a%d%m%Y-%H%M")
+
+# Log to a file
 logging.basicConfig(level=logging.INFO, filename=f'scrape_{stime}.log')
 
 # Uncomment to print logging info to stdout.
-logging.getLogger().addHandler(logging.StreamHandler(sys.stdout))
+#logging.getLogger().addHandler(logging.StreamHandler(sys.stdout))
 
 if __name__ == "__main__":
 	cli_entry()
